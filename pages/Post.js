18c9945b780bbe29fae8b85c875fd17c3133bd2a
Pedro Dias <pedrods@ipvc.pt>
2020-04-23 22:06:12 +0100

    Initial commit

---
+++ pages/Post.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Post js screen</Text>
+      </View>
+    );
+  }
+}
+
+export default Post;
