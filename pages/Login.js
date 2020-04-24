42eddd8d49501be25d1ff05392a340f0dbc8c21c
Pedro Dias <pedrods@ipvc.pt>
2020-04-24 19:14:10 +0100

    Basic login layout

--- pages/Login.js
+++ pages/Login.js
@@ -5,6 +5,10 @@ import {
   StyleSheet,
   ScrollView,
   View,
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
   Text,
   StatusBar,
   Button,
@@ -12,15 +16,91 @@ import {
 
 function Login({navigation}) {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Login Screen</Text>
-      <Button
-        title="Go to Register Screen"
-        onPress={() => navigation.navigate('Register')}
-      />
-      <Text />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Image source={require('../src/images/pointBRemoved.png')} />
+      </View>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TouchableOpacity style={styles.btnLogin}>
+          <Text style={styles.textLogin}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.textRegister}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLogo: {
+    flex: 1,
+    justifyContent: 'center',
+    aspectRatio: 0.75,
+    width: '20%',
+    height: '20%',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50,
+  },
+  input: {
+    backgroundColor: '#FFF',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7,
+  },
+  btnLogin: {
+    backgroundColor: '#1898cf',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
+  textLogin: {
+    color: '#FFF',
+    fontSize: 18,
+  },
+  btnRegister: {
+    backgroundColor: '#1898cf',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+    marginTop: 10,
+  },
+  textRegister: {
+    color: '#FFF',
+    fontSize: 18,
+    marginTop: 10,
+  },
+});
 
 export default Login;
