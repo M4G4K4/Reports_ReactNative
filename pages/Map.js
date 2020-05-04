150370461df5479bf57b5aecf018098d5c832649
Pedro Dias <pedrods@ipvc.pt>
2020-05-04 20:15:29 +0100

    Fix to show map

--- pages/Map.js
+++ pages/Map.js
@@ -1,19 +1,25 @@
 import React, {Component, useState} from 'react';
-import {Alert, FlatList, Text, TouchableOpacity, View} from 'react-native';
+import {Alert, FlatList, Text, TouchableOpacity, StyleSheet,View} from 'react-native';
 import MapView from 'react-native-maps';
 
 function Map({route, navigation}) {
   return (
-    <View>
-      <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    </View>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
 export default Map;
