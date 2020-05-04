f2526dc23bf1f8b65e682820b6237fb52992e6f9
Pedro Dias <pedrods@ipvc.pt>
2020-05-04 19:58:46 +0100

    Basic map shown

--- pages/Map.js
+++ pages/Map.js
@@ -1,12 +1,19 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
+import React, {Component, useState} from 'react';
+import {Alert, FlatList, Text, TouchableOpacity, View} from 'react-native';
+import MapView from 'react-native-maps';
 
-class Register extends Component {
-  render() {
-    return (
-        <Text>Maps</Text>
-    );
-  }
+function Map({route, navigation}) {
+  return (
+    <View>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
 }
-
-export default Register;
+export default Map;
