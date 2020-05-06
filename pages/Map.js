aa6f2367515f242a8ef8635287c7c8f64c6590ac
Pedro Dias <pedrods@ipvc.pt>
2020-05-06 20:56:51 +0100

    Showing markers in map

--- pages/Map.js
+++ pages/Map.js
@@ -7,24 +7,63 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker, Callout} from 'react-native-maps';
+import axios from 'axios';
+import {get} from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+let markersURL = 'https://www.emergency.wa.gov.au/data/incident_FCAD.json';
 
 // Main function
 function Map({route, navigation}) {
+  const [marker, setMarker] = useState([]);
+  const [call, setCall] = useState(true);
+
+  const getMarker = () => {
+    fetch('http://64.227.36.62/api/getAllReports')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setMarker(responseJson);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  if (call) {
+    getMarker();
+    setCall(false);
+  }
+
   return (
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  );
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 39.3325,
+          longitude: -7.5112,
+          latitudeDelta: 7,
+          longitudeDelta: 7,
+        }}>
+        {marker.map((marker) => (
+          <Marker
+            key={marker.ID}
+            coordinate={{
+              latitude: marker.longitude,
+              longitude: marker.latitude,
+            }}>
+            <Callout>
+              <Text>{marker.description}</Text>
+              <Text>{marker.morada}</Text>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+    </View>
+  ); // End return
 }
 
+
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
